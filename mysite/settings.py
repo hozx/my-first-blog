daaf548850896f53f8f718d7f941a277c45be037
hozx <daibarskyi@gmail.com>
2018-12-08 19:45:59 +0200

    q

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3qf!rrjf)w@ugd2hyzyj3v)4i-0v-)7j262!6lgoz474!o8##='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<hozx>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'hozx.pythonanywhere.com']
 
 
 # Application definition
